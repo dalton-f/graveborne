e577daf4038fb96df34129256412e7a34af38e22
Dalton <daltonfiggdavies@gmail.com>
2025-11-09 10:47:17 +0000

    Create a finite state manager
    Create the FSM script as well as a state script to use for the player and NPCs

---
+++ common/managers/finite_state_manager/finite_state_manager.gd
@@ -0,0 +1,48 @@
+extends Node
+class_name FiniteStateManager
+
+signal state_changed(new_state_name : String)
+
+@export var initial_state : State
+
+var states : Dictionary = {}
+var current_state : State
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			child.state_transition.connect(change_state)
+
+	if initial_state:
+		initial_state.enter()
+		current_state = initial_state
+
+func _process(delta):
+	if current_state:
+		current_state.update(delta)
+
+func _physics_process(delta: float) -> void:
+	if current_state:
+		current_state.physics_update(delta)
+
+#region State Management
+func change_state(source_state : State, new_state_name : String):
+	if source_state != current_state:
+		print("Invalid change_state trying from: " + source_state.name + " but currently in: " + current_state.name)
+		return
+	
+	var new_state = states.get(new_state_name.to_lower())
+	
+	if !new_state:
+		print("New state is empty")
+		return
+		
+	if current_state:
+		current_state.exit()
+		
+	new_state.enter()
+	
+	current_state = new_state
+	state_changed.emit(new_state_name)
+#endregion
---
+++ common/managers/finite_state_manager/state.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name State
+
+@warning_ignore("unused_signal")
+signal state_transition
+
+func enter():
+	pass
+	
+func exit():
+	pass
+	
+func update(_delta: float):
+	pass
+
+func physics_update(_delta: float):
+	pass
